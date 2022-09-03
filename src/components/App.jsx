a41401ec520bce6d71f3289acba031710acf8960
PatrycjaWiza <95050810+PatrycjaWiza@users.noreply.github.com>
2022-09-03 23:40:55 +0200

    storage update check

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,14 +11,14 @@ export const App = () => {
   useEffect(() => {
     const contactsStored = localStorage.getItem('contacts');
     const storageContacts = JSON.parse(contactsStored);
-    // storageContacts !== [] ?
-    setContacts(storageContacts);
-    // : setContacts([
-    //     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    //     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    //     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    //     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    //   ]);
+    storageContacts !== []
+      ? setContacts(storageContacts)
+      : setContacts([
+          { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+          { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+          { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+          { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+        ]);
   }, []);
 
   useEffect(() => {
